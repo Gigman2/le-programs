a6922be49170b11bbf862a10daac9c5591bfe416
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2023-10-08 18:46:49 +0000

    overall view

--- src/pages/bus/overall-head/index.tsx
+++ src/pages/bus/overall-head/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
-import { Box, Flex, Icon, Skeleton, Text } from '@chakra-ui/react'
+import { Box, Divider, Flex, Icon, Skeleton, Text } from '@chakra-ui/react'
 import { IAccountUser, getUser, removeSession, saveBusUser } from '@/frontend/store/auth'
 import { useRouter } from 'next/router'
 import { BsPersonFillAdd } from 'react-icons/bs'
@@ -42,8 +42,67 @@ export default function OverallhHead() {
         </Flex>
 
         <Box mt={4}>
-          
+          <Flex gap={3}>
+            <Box bg="gray.100" p={4} rounded={"md"} flex={7}>
+              <Text color={"gray.400"}>Zones</Text>
+              <hr />
+              <Flex align={"baseline"} color={"gray.500"} gap={2}>
+                <Text fontSize={32} fontWeight={600}>265</Text>
+                <Text>zones bused</Text>
+              </Flex>
+              <Flex align={"baseline"} color={"gray.400"} gap={2}>
+                <Text fontWeight={700}>48</Text>
+                <Text>zones have not bused</Text>
+              </Flex>
+            </Box>
+            <Box bg="blue.100" p={4} rounded={"md"} flex={6}>
+                <Box borderColor={"blue.200"} borderBottomWidth={1} color={"blue.400"}>Bus</Box>
+                <Flex align={"baseline"} color={"blue.500"} gap={2}>
+                <Text fontSize={32} fontWeight={600}>391</Text>
+                  <Text>on route</Text>
+                </Flex>
+                <Flex align={"baseline"} color={"blue.400"} gap={2}>
+                <Text fontWeight={700}>48</Text>
+                <Text>buses have arrived</Text>
+              </Flex>
+            </Box>
+          </Flex>
+          <Flex gap={3} mt={3}>
+            <Box bg="blue.100" p={4} rounded={"md"} flex={6}>
+                <Box borderColor={"blue.200"} borderBottomWidth={1} color={"blue.400"}>People</Box>
+                <Flex align={"baseline"} color={"blue.500"} gap={2}>
+                <Text fontSize={32} fontWeight={600}>1234</Text>
+                  <Text>on route</Text>
+                </Flex>
+                <Flex align={"baseline"} color={"blue.400"} gap={2}>
+                <Text fontWeight={700}>515</Text>
+                <Text>people have arrived</Text>
+              </Flex>
+            </Box>
+            <Box bg="gray.100" p={4} rounded={"md"} flex={7}>
+              <Text color={"gray.400"}>Financial</Text>
+              <hr />
+              <Flex align={"baseline"} color={"gray.500"} gap={2}>
+                <Text fontWeight={600}>Ghc </Text>
+                <Text fontSize={28} fontWeight={600}>2265</Text>
+                <Text>bus offering</Text>
+              </Flex>
+              <Flex align={"baseline"} color={"gray.400"} gap={2}>
+                <Text fontWeight={700}>Ghc 20000</Text>
+                <Text>busing cost</Text>
+              </Flex>
+            </Box>
+          </Flex>
         </Box>
+        
+        <Flex rounded={"md"} mt={4} p={4} bg="yellow.100" gap={4} align={"center"}>
+          <Text fontWeight={600} color={"yellow.600"} fontSize={24}>21%</Text>
+          <Box>
+            <Text color={"yellow.500"}>had more than 15 people in bus</Text>
+            <Divider />
+            <Text color={"yellow.500"}>237 / 451 buses</Text>
+          </Box>
+        </Flex>
       </AppWrapper>
     </GuardWrapper>
   )
