2dc2b1ae2d9c0d93327ead9ae586863e5b9fd9fc
Eric Abbey <abbeykojoeric@gmail.com>
2024-11-02 11:52:42 +0000

    updating the structures

--- src/pages/ushers/add.tsx
+++ src/pages/ushers/add.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
-import { Box, Button, Flex, FormLabel, Grid, Input, Text, useToast, Icon } from '@chakra-ui/react'
+import { Box, Button, Flex, FormLabel, Grid, Input, Text, useToast, Icon, AccordionItem, Accordion, AccordionButton, AccordionPanel, AccordionIcon} from '@chakra-ui/react'
 import { handleChange } from '@/utils/form'
 import { getUser } from '@/utils/auth'
 import moment from 'moment'
@@ -14,27 +14,43 @@ export default function Home() {
   const [loading, setLoading] = useState(false)
   const [currentUser, setCurrentUser] = useState<{name?: string;}>({})
   const [auditorium, setAuditorium] = useState<Record<string, number>>({
-    'choir': 0,
-    'behind choir': 0,
-    'ext main left 1': 0,
-    'ext main left 2': 0,
-    'main left 1': 0,
-    'main left 2': 0,
-    'main center 1': 0,
-    'main center 2': 0,
-    'main right 1': 0,
-    'main right 2': 0,
-    'ext main right 1': 0,
-    'ext main right 2': 0,
-    'behind mc': 0,
-    'mc Heads': 0,
+    'A': 0,
+    'C1': 0,
+    'C2': 0,
+    'B': 0,
+    'D1': 0,
+    'D2': 0,
+    'ext main left': 0,
+    'main left': 0,
+    'main center': 0,
+    'main right': 0,
+    'ext main right': 0,
     'media top': 0,
-    'audio room': 0,
-    'pastors lounge': 0,
-    'mothers lounge': 0,
-    'kids': 0,
-    'miscellaneous': 0, 
   })
+
+  const [backOffice, setBackOffice] = useState<Record<string, number>>({
+    'MCR': 0,
+    "Pastor's Lounge": 0,
+    "Mother's Lounge": 0,
+    "Finance": 0,
+    "Hallway": 0,
+    "Ushers": 0,
+    "kids": 0,
+  })
+
+  const [overflow, setOverflow] = useState<Record<string, number>>({
+    "Overflow 1": 0,
+    "Overflow 2": 0,
+    "Overflow 3": 0,
+  })
+
+  const [annex, setAnnex] = useState<Record<string, number>>({  
+    "Annex 1": 0,
+    "Annex 2": 0,
+    "Annex 3": 0,
+  })
+
+
 
     const toastMessage: { 
       title: string; 
@@ -53,7 +69,7 @@ export default function Home() {
   const handleSubmit = async () => {
     try {
       setLoading(true)
-      const data = {...auditorium}
+      const data = {...auditorium, ...backOffice, ...overflow, ...annex}
       const total = Object.keys(data).reduce((acc, cur) => {
         acc += Number(data[cur])
         return acc
@@ -78,7 +94,13 @@ export default function Home() {
   const resetData = () => {
     const auditoriumData = {...auditorium}
     Object.keys(auditoriumData).map(item => auditoriumData[item] = 0)
+    Object.keys(backOffice).map(item => backOffice[item] = 0)
+    Object.keys(overflow).map(item => overflow[item] = 0)
+    Object.keys(annex).map(item => annex[item] = 0)
     setAuditorium(auditoriumData)
+    setBackOffice(backOffice)
+    setOverflow(overflow)
+    setAnnex(annex)
   } 
 
   useEffect(() => {
@@ -105,7 +127,7 @@ export default function Home() {
                 </Box>
             </Flex>
 
-           <Box mt={4}>
+            <Box mt={4}>
               <Flex justify={"space-between"}>
                 <Flex 
                 onClick={() => router.push('/ushers')}
@@ -124,22 +146,106 @@ export default function Home() {
                     Go Back
                 </Flex>
             </Flex>
-              <Box borderBottomWidth={2} borderColor={"gray.300"}>
-                <Text fontWeight={600} fontSize={18} color={"gray.500"}>Auditorium</Text>
+            <Accordion allowToggle>
+              <AccordionItem>
+                <AccordionButton _expanded={{ bg: 'blue.500', color: 'white' }}>
+                  <Box as="span" flex='1' textAlign={'left'} fontWeight={600} fontSize={18}>Add Count for Back Auditorium</Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                  <Box>
+                    <Grid templateColumns="repeat(3,1fr)" columnGap={6} rowGap={3}>
+                      {Object.keys(auditorium).map(item => 
+                        <Box key={item} borderWidth={1} borderColor={"gray.200"} rounded="md" p={2} mb={4}>
+                          <FormLabel fontSize={14} textTransform="capitalize">{item}</FormLabel>
+                          <Input 
+                              type={"text"}
+                              name="firstName"
+                              placeholder='Enter name here ...' 
+                              value={auditorium[item]} 
+                              onChange={(v) => handleChange(v?.currentTarget?.value, item, auditorium, setAuditorium)} 
+                          />
+                      </Box>)}
+                    </Grid>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <AccordionButton  _expanded={{ bg: 'blue.500', color: 'white' }}>
+                  <Box as="span" flex='1' textAlign={'left'} fontWeight={600} fontSize={18}>Add Count for Back Office</Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                <Box mt={4}>
+                <Text fontWeight={600} fontSize={18} color={"gray.500"}>Back Office</Text>
                 <Grid templateColumns="repeat(3,1fr)" columnGap={6} rowGap={3}>
-                  {Object.keys(auditorium).map(item => 
+                  {Object.keys(backOffice).map(item => 
                     <Box key={item} borderWidth={1} borderColor={"gray.200"} rounded="md" p={2} mb={4}>
                       <FormLabel fontSize={14} textTransform="capitalize">{item}</FormLabel>
                       <Input 
                           type={"text"}
                           name="firstName"
                           placeholder='Enter name here ...' 
-                          value={auditorium[item]} 
-                          onChange={(v) => handleChange(v?.currentTarget?.value, item, auditorium, setAuditorium)} 
+                          value={backOffice[item]} 
+                          onChange={(v) => handleChange(v?.currentTarget?.value, item, backOffice, setBackOffice)} 
                       />
-                  </Box>)}
-                </Grid>
-              </Box>
+                    </Box>)}
+                    </Grid>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <AccordionButton _expanded={{ bg: 'blue.500', color: 'white' }}>
+                  <Box as="span" flex='1' textAlign={'left'} fontWeight={600} fontSize={18}>Add Count for Back Overflow</Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                <Box mt={4}>
+                <Text fontWeight={600} fontSize={18} color={"gray.500"}>Overflow</Text>
+                <Grid templateColumns="repeat(3,1fr)" columnGap={6} rowGap={3}>
+                  {Object.keys(overflow).map(item => 
+                    <Box key={item} borderWidth={1} borderColor={"gray.200"} rounded="md" p={2} mb={4}>
+                      <FormLabel fontSize={14} textTransform="capitalize">{item}</FormLabel>
+                      <Input 
+                          type={"text"}
+                          name="firstName"
+                          placeholder='Enter name here ...' 
+                          value={overflow[item]} 
+                          onChange={(v) => handleChange(v?.currentTarget?.value, item, overflow, setOverflow)} 
+                      />
+                    </Box>)}
+                    </Grid>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <AccordionButton _expanded={{ bg: 'blue.500', color: 'white' }}>
+                  <Box as="span" flex='1' textAlign={'left'} fontWeight={600} fontSize={18}>Add Count for Back Annex</Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                <Box mt={4}>
+                <Text fontWeight={600} fontSize={18} color={"gray.500"}>Overflow</Text>
+                <Grid templateColumns="repeat(3,1fr)" columnGap={6} rowGap={3}>
+                  {Object.keys(annex).map(item => 
+                    <Box key={item} borderWidth={1} borderColor={"gray.200"} rounded="md" p={2} mb={4}>
+                      <FormLabel fontSize={14} textTransform="capitalize">{item}</FormLabel>
+                      <Input 
+                          type={"text"}
+                          name="firstName"
+                          placeholder='Enter name here ...' 
+                          value={annex[item]} 
+                          onChange={(v) => handleChange(v?.currentTarget?.value, item, annex, setAnnex)} 
+                      />
+                    </Box>)}
+                    </Grid>
+                  </Box>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
               <Box as={Button} 
                   width="full" 
                   mt={24} 
--- src/pages/ushers/index.tsx
+++ src/pages/ushers/index.tsx
@@ -76,101 +76,40 @@ export default function Home() {
                 >Add Headcount</Box>
             </Flex>
 
-           <Box mt={4}>
-                {headCounts.map(item =>  (<Box 
-                  key={item._id as string} textAlign={'center'} fontWeight={600} color='gray.600' p={2} borderWidth={1} borderColor={"gray.200"} rounded="md" mb={8}>
-                    <Flex align="center" justify={"space-between"}>
-                        <Text fontSize={15} fontWeight={700} color="gray.600">Total <Text as="span" fontSize={20} ml={2}>{item.total as string}</Text></Text>
-                        <Text fontSize={15} fontWeight={700} color="gray.600">{moment(item.created_on as string).format('h: mm A')}</Text>
+          <Box mt={4}>
+              {headCounts.map(item =>  (<Box 
+                key={item._id as string} textAlign={'center'} fontWeight={600} color='gray.600' p={2} borderWidth={1} borderColor={"gray.200"} rounded="md" mb={8}>
+                  <Flex align="center" justify={"space-between"}>
+                      <Text fontSize={15} fontWeight={700} color="gray.600">Total <Text as="span" fontSize={20} ml={2}>{item.total as string}</Text></Text>
+                      <Text fontSize={15} fontWeight={700} color="gray.600">{moment(item.created_on as string).format('h: mm A')}</Text>
+                  </Flex>
+                  <Flex  mt={2} align={"center"} borderBottomWidth={1} borderColor="gray.200" my={1}>
+                    <Text fontSize={14} mr={2}>Recorded by </Text>
+                    <Text fontWeight={600} textTransform={'capitalize'}>{item.recorder as string}</Text>
+                  </Flex>
+                  <Box mt={2}>
+                    <Flex bg="orange.100" p={3} rounded={"md"} mb={2} w={"84%"}>
+                      
                     </Flex>
-                    <Flex  mt={2} align={"center"} borderBottomWidth={1} borderColor="gray.200" my={1}>
-                      <Text fontSize={14} mr={2}>Recorded by </Text>
-                      <Text fontWeight={600} textTransform={'capitalize'}>{item.recorder as string}</Text>
+                    <Flex gap={2} rounded={"md"}>
+                      <Flex w="85%" bg={"purple.200"} direction={"column"} p={2} rounded={"md"}>
+                        
+                      </Flex>
+                      <Flex direction={"column"} w="15%" borderWidth={1} borderColor={"gray.300"} bg="pink.200" rounded="md" overflow={"hidden"}>
+                        
+                      </Flex>
                     </Flex>
-                    <Box mt={2}>
-                      <Flex bg="orange.100" p={3} rounded={"md"} mb={2} w={"84%"}>
-                        {Number(item.section['pastors lounge'] || 0) + Number(item.section['audio room'] || 0) + Number(item.section['office hallway'] || 0)}
-                      </Flex>
-                      <Flex gap={2} rounded={"md"}>
-                        <Flex w="85%" bg={"purple.200"} direction={"column"} p={2} rounded={"md"}>
-                          <Flex mb={3} justifyContent={'space-between'}>
-                            <Flex gap={2}>
-                              <Box w={20} px={2} py={0.5} borderColor={'gray.200'}  bg='green.100' borderWidth={1} rounded={'md'}>
-                                {item.section['behind choir']}
-                              </Box>
-                              <Box w={20} px={2} py={0.5} borderColor={'gray.200'} bg='green.100' borderWidth={1} rounded={'md'}>
-                                {item.section['choir']}
-                              </Box>
-                            </Flex>
-                            <Flex gap={2}>
-                              <Box w={16} px={2} py={1} borderColor={'gray.200'} bg='green.100' borderWidth={1} rounded={'md'}>
-                                {item.section['mc Heads']}
-                              </Box>
-                              <Box w={14} px={2} py={1} borderColor={'gray.200'} bg='green.100' borderWidth={1} rounded={'md'}>
-                                {item.section['behind mc']}
-                              </Box>
-                            </Flex>
-                          </Flex>
-                          <Flex gap={2}>
-                            <Box flex={1} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['ext main left 1']}
-                            </Box>
-                            <Box flex={3} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['main left 1']}
-                            </Box>
-                            <Box flex={5} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['main center 1']}
-                            </Box>
-                            <Box flex={3} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['main right 1']}
-                            </Box>
-                            <Box flex={1} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['ext main right 1']}
-                            </Box>
-                          </Flex>
-                          <Flex gap={2} mt={2}>
-                            <Box flex={1} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['ext main left 2']}
-                            </Box>
-                            <Box flex={3} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['main left 2']}
-                            </Box>
-                            <Box flex={5} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['main center 2']}
-                            </Box>
-                            <Box flex={3} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['main right 2']}
-                            </Box>
-                            <Box flex={1} px={2} py={1} borderColor={'gray.200'} bg='orange.100' borderWidth={1} rounded={'md'}>
-                              {item.section['ext main right 2']}
-                            </Box>
-                          </Flex>
-                          <Flex justify={'center'}>
-                            <Box>
-                              <Box w={20} my={2} px={2} py={1} bg={'gray.200'} rounded={'md'}>
-                                {item.section['media top']}
-                              </Box>
-                            </Box>
-                          </Flex>
-                        </Flex>
-                        <Flex direction={"column"} w="15%" borderWidth={1} borderColor={"gray.300"} bg="white.200" rounded="md" overflow={"hidden"}>
-                          <Flex align={'center'} justify={"center"} w="100%" h="50%" p={2} bg="pink.200">
-                            {Number(item.section['mothers lounge'] || 0) + Number(item.section['mother lounge 2'] || 0)}
-                          </Flex>
-                        </Flex>
-                      </Flex>
-                    </Box>
-                    <Grid mt={1} templateColumns="repeat(2,1fr)" py={2} columnGap={12} rowGap={0} borderTopWidth={1} borderColor={'gray.200'}>
-                      {Object.keys(item.section).filter(key => !defaultSections.includes(key)).map((s: string) => (
-                        <Flex key={s} mr={3} align="center" justify={"space-between"}>
-                          <Text fontSize={13} mr={2} textTransform={'capitalize'}>{s}</Text> 
-                          <Text fontWeight={600}>{(item.section as unknown as Record<string, string>)?.[s]}</Text>
-                      </Flex>
-                      ))}
-                    </Grid>
-                </Box>))}
-               
-            </Box>
+                  </Box>
+                  <Grid mt={1} templateColumns="repeat(2,1fr)" py={2} columnGap={12} rowGap={0} borderTopWidth={1} borderColor={'gray.200'}>
+                    {Object.keys(item.section).filter(key => !defaultSections.includes(key)).map((s: string) => (
+                      <Flex key={s} mr={3} align="center" justify={"space-between"}>
+                        <Text fontSize={13} mr={2} textTransform={'capitalize'}>{s}</Text> 
+                        <Text fontWeight={600}>{(item.section as unknown as Record<string, string>)?.[s]}</Text>
+                    </Flex>
+                    ))}
+                  </Grid>
+              </Box>))}
+          </Box>
           </Box>
         </Flex>
       </main>
